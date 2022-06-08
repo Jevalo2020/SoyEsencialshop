8eef3a403c236eb48aef58a7f487d4d1aa4e420a
jevalo2020 <jevalo2020@gmail.com>
2022-06-08 04:11:18 -0500

    js app updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bienvenido a SoyEsencial
         </a>
       </header>
     </div>
